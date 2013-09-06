d051f4558ba6235f13c3d1809e6d94496cb12d65
David Hooey <davidh@opentext.com>
2013-09-06 14:43:06 -0400

    Removing USER_ERRORS select as it's failing.

--- RunSQL.sql
+++ RunSQL.sql
@@ -4,9 +4,9 @@ SET VERIFY OFF
 SET FEEDBACK OFF
 @&1
 SELECT 'Filename: &1' FROM DUAL;
-SELECT '(' || TYPE || ' ' || NAME || '/0:' || POSITION || ')' || ' ' || LINE || ':' || POSITION || ' ' || TEXT as ERRORS
-  FROM USER_ERRORS
- WHERE line <> 0
-   AND TYPE || ' ' || NAME in (&2)
-ORDER BY NAME, TYPE, SEQUENCE;
+-- SELECT '(' || TYPE || ' ' || NAME || '/0:' || POSITION || ')' || ' ' || LINE || ':' || POSITION || ' ' || TEXT as ERRORS
+--   FROM USER_ERRORS
+--  WHERE line <> 0
+--    AND TYPE || ' ' || NAME in (&2)
+-- ORDER BY NAME, TYPE, SEQUENCE;
 exit
