1928c48aa37d31911b679958fff0bc939cbf4a08
mezdm <mezhuev@gmail.com>
2014-02-03 16:32:37 +0200

    improve build system, add some missing keywords

--- RunSQL.sql
+++ RunSQL.sql
@@ -8,11 +8,6 @@ SET FEEDBACK OFF
 SELECT 'Filename: &1' FROM DUAL
 /
 
-SELECT '(' || type || ' ' || name || '/0:' || position || ')' || ' ' || line || ':' || position || ' ' || text as errors
-  FROM user_errorS
- WHERE line <> 0
-   AND type || ' ' || name in (&2)
-ORDER BY name, type, sequence
-/
+SHOW ERRORS
 
 EXIT
--- oracle_exec.py
+++ oracle_exec.py
@@ -9,34 +9,32 @@ RE_ENTITIES = re.compile("^\\((.+?)/(0):[0-9]+\\) ([0-9]+):[0-9]+ (.+)$", re.M)
 
 class OracleExecCommand(execmod.ExecCommand):
     def run(self, dsn="", **kwargs):
-        print(dsn)
-        print(kwargs)
         if not dsn and not kwargs.get("kill", False):
             # if cmd is empty, open the command_palette with the available build list
             self.window.run_command("show_overlay", {"overlay": "command_palette", "text": "Build: " + kwargs.get("prefix", "")})
         else:
             # Find entities declaration in source
             self.entities = oracle_lib.find_entities(self.window.active_view())
-            # Create a string for the in of sql command
-            sqlfilter = '"' + ",".join("'%s'" % entity for entity in self.entities.keys()) + '"'
 
             (directory, filename) = os.path.split(self.window.active_view().file_name())
-            cmd = ["sqlplus.exe", "-s", dsn, "@", os.path.join(sublime.packages_path(), 'OracleSQL', 'RunSQL.sql'), '"'+filename+'"', sqlfilter]
+            cmd = ["sqlplus", "-s", dsn, "@", os.path.join(sublime.packages_path(), 'OracleSQL', 'RunSQL.sql'), '"'+filename+'"']
 
-            super(OracleExecCommand, self).run(cmd, None, "^Filename: (.+)$", "^\\(.+?/([0-9]+):([0-9]+)\\) [0-9]+:[0-9]+ (.+)$", working_dir=directory, **kwargs)
+            super(OracleExecCommand, self).run(cmd, None, "^Filename: (.+)$", "^([0-9]+)/([0-9]+) (.+)$", working_dir=directory, **kwargs)
 
-    def append_string(self, proc, data):
-        # Update the line number of output_view with the correct line number of source view
-        orgdata = data
+    def append_string(self, proc, string):
+        """ Update the line number of output_view with the correct line number of source view
+        """
+        orgstring = string
         posoffset = 0
-        for re_ent in RE_ENTITIES.finditer(orgdata):
+
+        for re_ent in RE_ENTITIES.finditer(orgstring):
             pos = re_ent.span(2)
             pos = (pos[0] + posoffset, pos[1] + posoffset)
             sourceoffset = self.entities[re_ent.group(1)]
             sqlerrorline = int(re_ent.group(3))
             sourceline = sqlerrorline + sourceoffset
 
-            data = data[:pos[0]] + str(sourceline) + data[pos[1]:]
+            string = string[:pos[0]] + str(sourceline) + string[pos[1]:]
             posoffset += len(str(sourceline)) - 1
 
-        super(OracleExecCommand, self).append_string(proc, data)
+        super(OracleExecCommand, self).append_string(proc, string)
